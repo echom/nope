b39c8f00f31c20035b6b7a6d636072fed22c9772
Jörg Hösel <echom@gmx.net>
2016-02-19 18:13:32 +0100

    redoing the entire thing...

--- test/tutorial.js
+++ test/tutorial.js
@@ -1,13 +1,27 @@
-tutorial = np.html()
-  .head()
-    .meta('', {charset: 'utf-8'}).up()
-    .title('nope.js - tutorial').up()
-    .link('', {type: 'stylesheet', rel: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css' }).up()
-  .up()
-  .body()
-    .h1('nope.js - tutorial').up()
-    .div()
-      .h2('Getting started').up()
-      .p('').up()
-    .up()
-  .up()
+var data = new Data();
+
+np.html(html => html.html([
+  html.head([
+    html.meta({charset: 'utf-8'}),
+    html.title('nope.js - tutorial'),
+    html.link({
+      type: 'stylesheet',
+      rel: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css'
+    })
+  ]),
+  html.body([
+    html.h1('nope.js - tutorial'),
+    html.div([
+      html.h2('Quick Start'),
+      html.p('First things first!').style(np.style(css => [
+        css.position('absolute'),
+        css.top('10px')
+      ])).text(np.bind(() => data.quickstart))
+    ])
+    html.component(c => c.chapters({ data: Data.chapters })),
+    np.each(np.bind(() => data.chapters), (index, item) => [
+      html.h2(item.title),
+      html.p([item.content.substr(0, 300), "..."].join())
+    ]);
+  ])
+]));
